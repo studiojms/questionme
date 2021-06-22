20efc6003c91a559e9778a00f8c9920c1564482c
Jefferson M Souza <jefferson.msouza86@gmail.com>
2021-06-21 23:09:02 -0300

    working with context and firebase authentication

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,17 @@ import { BrowserRouter, Route } from 'react-router-dom';
 import { Home } from './pages/Home';
 import { NewRoom } from './pages/NewRoom';
 
+import { AuthContextProvider } from './contexts/AuthContext';
+
 import './styles/global.scss';
 
 function App() {
   return (
     <BrowserRouter>
-      <Route path="/" exact component={Home} />
-      <Route path="/rooms/new" exact component={NewRoom} />
+      <AuthContextProvider>
+        <Route path="/" exact component={Home} />
+        <Route path="/rooms/new" exact component={NewRoom} />
+      </AuthContextProvider>
     </BrowserRouter>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,22 @@
 import { useHistory } from 'react-router-dom';
+
 import googleIconImg from '../assets/images/google-icon.svg';
 import illustrationImg from '../assets/images/illustration.svg';
 import logoImg from '../assets/images/logo.svg';
 import { Button } from '../components/Button';
+import { useAuth } from '../hooks/useAuth';
 
 import '../styles/auth.scss';
 
 export function Home() {
   const history = useHistory();
+  const { user, signInWithGoogle } = useAuth();
 
-  function goToNewRoom() {
+  async function goToNewRoom() {
+    if (!user) {
+      await signInWithGoogle();
+    }
+
     history.push('/rooms/new');
   }
 
